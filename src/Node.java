d270623e05dacb3c743339903ec31b5d3fdbbfd9
aslikarahan <asikarahan96@gmail.com>
2020-05-29 14:08:59 +0300

    project initialization

---
+++ src/Node.java
@@ -0,0 +1,19 @@
+import java.util.Hashtable;
+
+public class Node extends Thread {
+    public int nodeID;
+    public Hashtable<Integer, Integer> linkCost; //Neighbor id, link cost
+    public Hashtable<Integer, Integer> linkBandwidth; //Neighbor id, link bandwidth
+    public int[][] distanceTable;
+    public int[] bottleneckBandwidthTable;
+    public final int infinite = 999;
+
+
+    public Node(int nodeID, Hashtable<Integer, Integer> linkCost, Hashtable<Integer, Integer> linkBandwidth) {
+        this.nodeID = nodeID;
+        this.linkCost = linkCost;
+        this.linkBandwidth = linkBandwidth;
+
+
+    }
+}
