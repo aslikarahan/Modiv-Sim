9442298157de78de837e758ae3ef09cc40222630
aslikarahan <asikarahan96@gmail.com>
2020-05-29 18:22:12 +0300

    structure formed

--- src/ModivSim.java
+++ src/ModivSim.java
@@ -1,3 +1,69 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.Scanner;
+
 public class ModivSim {
+    private static ModivSim singletonInstance;
+    public ArrayList<Node> nodes = new ArrayList<>();
+    private ModivSim() {
+    }
+    public static ModivSim getInstance() {
+        if(singletonInstance == null) {
+            synchronized (ModivSim.class) {
+                if(singletonInstance == null) {
+                    singletonInstance = new ModivSim();
+                }
+            }
+        }
+        return singletonInstance;
+    }
+    
+    public void readInputFile(String directory){
+        File dir = new File(directory);
+        File[] files = dir.listFiles(new FilenameFilter() {
+            public boolean accept(File dir, String filename) {
+                return filename.toLowerCase().endsWith(".txt");
+            }
+        });
+        for(File f : files){
+            try {
+                FileInputStream fis=new FileInputStream(directory +"/" +f.getName());
+                Scanner sc=new Scanner(fis);
+                while(sc.hasNextLine()) {
+                    String[] temp = sc.nextLine().split("[\\(||\\)||,]");
+                    ArrayList<Integer> parameters = new ArrayList<Integer>();
+                    for(String s : temp){
+                        if (s.length()!=0){
+                            parameters.add(Integer.parseInt(s));
+                        }
+                    }
+                    int numberOfNodes = parameters.size() / 3;
+                    int nodeID = parameters.get(0);
+                    Hashtable<Integer, Integer> linkCost = new Hashtable<Integer, Integer>();
+                    Hashtable<Integer, Integer> linkBandwidth = new Hashtable<Integer, Integer>();
+                    for(int i = 0; i < numberOfNodes; i ++){
+                        int neighborID = parameters.get(3 * i + 1);
+                        int neighborLinkCost = parameters.get(3 * i + 2);
+                        int neighborBandwidth = parameters.get(3 * i + 3);
+                        linkCost.put(neighborID, neighborLinkCost);
+                        linkBandwidth.put(neighborID, neighborBandwidth);
+                    }
+                    nodes.add(new Node(nodeID, linkCost, linkBandwidth));
+                    }
+                sc.close();
+            } catch(IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+    }
+
+    public void startNodeThreads(){
+        for(Node n : this.nodes){
+            n.start();
+        }
+    }
 
 }
--- src/Node.java
+++ src/Node.java
@@ -1,18 +1,49 @@
 import java.util.Hashtable;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
 
-public class Node {
+public class Node extends Thread{
     public int nodeID;
     public Hashtable<Integer, Integer> linkCost; //Neighbor id, link cost
     public Hashtable<Integer, Integer> linkBandwidth; //Neighbor id, link bandwidth
     public int[][] distanceTable;
     public int[] bottleneckBandwidthTable;
     public final int infinite = 999;
-
+    final ScheduledExecutorService scheduler =
+            Executors.newScheduledThreadPool(1);
 
     public Node(int nodeID, Hashtable<Integer, Integer> linkCost, Hashtable<Integer, Integer> linkBandwidth) {
         this.nodeID = nodeID;
         this.linkCost = linkCost;
         this.linkBandwidth = linkBandwidth;
     }
+    public void receiveUpdate(Message m){
+
+    }
+    public void run(){
+        startScheduling();
+    }
+
+    public boolean sendUpdate(){
+
+        return true;
+    }
+    public Hashtable<Integer, Integer[]> getForwardingTable(){
+
+        return null;
+    }
+    public void startScheduling(){
+        ScheduledFuture scheduledFuture =
+                scheduler.scheduleAtFixedRate(new Runnable() {
+                                                  public void run(){
+                                                      sendUpdate();
+                                                  }
+                                              },
+                        5,30,
+                        TimeUnit.SECONDS);
+    }
+
 
 }
